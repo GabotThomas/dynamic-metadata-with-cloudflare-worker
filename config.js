ac3fa40709ef63cd772567e5569cbd4452e52215
GabotThomas <gabotthomas@gmail.com>
2024-11-15 19:04:22 +0100

    test config

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
-  patterns: [
-      {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-      }
-      // Add more patterns and their metadata endpoints as needed
-  ]
+	domainSource: 'https://www.frenchdetailers.com', // Your WeWeb app preview link
+	patterns: [
+		{
+			pattern: '/company/[^/]+',
+			metaDataEndpoint:
+				'https://ljplutjhayfqdoxssdri.supabase.co/rest/v1/public_view_metadata_companies?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcGx1dGpoYXlmcWRveHNzZHJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2ODc5NjQsImV4cCI6MjAyNjI2Mzk2NH0.k1osx2d-JHlBAHuv4niOCVPt50SFtr2FhdCFdvoBfk8&select=title,description,image,keywords&or=%28and%28id.eq.%22{id}%22%29%29',
+		},
+	],
 };
